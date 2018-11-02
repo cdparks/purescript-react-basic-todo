8d8545d2932c03a0444412e9efc1d795d889ba5c
Chris Parks <christopher.daniel.parks@gmail.com>
2018-11-02 11:51:21 +0000

    Summarize tasks in console on load

--- src/purs/Main.purs
+++ src/purs/Main.purs
@@ -5,13 +5,22 @@ module Main
 import Prelude
 
 import Browser.Storage as Storage
+import Components.App as App
+import Data.Array as Array
 import Data.Maybe (fromMaybe)
 import Effect (Effect)
+import Effect.Console as Console
 import React.Render (renderTo)
-
-import Components.App as App
 
 main :: Effect Unit
 main = do
   tasks <- fromMaybe [] <$> Storage.getItem "tasks"
+  Console.log $ summarize tasks
   renderTo "root" App.component {initialTasks: tasks}
+
+summarize :: forall fields. Array { completed :: Boolean | fields } -> String
+summarize tasks =
+  show numCompleted <> "/" <> show numTotal <> " completed"
+ where
+  numTotal = Array.length tasks
+  numCompleted = Array.length $ Array.filter _.completed tasks
